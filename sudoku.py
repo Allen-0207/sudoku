88d5439125f962921b78d375827ee6aa9734751e
Allen_Luo <box5519@gmail.com>
2023-08-29 22:40:12 +0800

    Update sudoku.py
    讓視窗顯示在畫面中間

--- sudoku.py
+++ sudoku.py
@@ -4,16 +4,21 @@ import tkinter as tk
 from Board import SudokuUI
 from generate import Game
 
-
+w = 700
+h = 500
 
 if __name__ == '__main__':
     window = tk.Tk()
-
+    ws = window.winfo_screenwidth() # width of the screen
+    hs = window.winfo_screenheight() # height of the screen
     game = Game()    
         
     board = SudokuUI(window, game)
-    
-    window.geometry('700x700')
+
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+
+    window.geometry('%dx%d+%d+%d' % (w, h, x, y))
     window.resizable(False, False) #Fix window size
 
     window.mainloop()   
