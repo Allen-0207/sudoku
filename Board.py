ed4f96682cafb4b9443246b298feda7af7b547b9
Allen_Luo <box5519@gmail.com>
2023-09-01 21:48:32 +0800

    Update Board.py
    Add time, victory

--- Board.py
+++ Board.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
  
 
 MARGIN = 20  # Pixels around the board
@@ -9,6 +10,8 @@ BTN_WIDTH, BTN_HEIGHT = 8, 2
 BTN_PADX = 20
 BTN_FONT = ("Comic Sans MS", 20, "bold")
 CELL_FONT = ("Comic Sans MS", 16, "bold")
+WIN_FONT = ("Noto Sans CJK JP", 36, "bold")
+WIN_FONT2 = ("Noto Sans CJK JP", 18)
 
 
 class SudokuUI(tk.Frame):
@@ -17,29 +20,34 @@ class SudokuUI(tk.Frame):
         self.window = window
         self.game = game
         self.row, self.col = -1, -1 #select cell, (-1, -1): not selects
+        self.time = 0
+        self.timer = None
+        self.diff = "Easy"
         self.initUI()
 
     def initUI(self):
         self.window.title("Sudoku")
         # self.pack(fill=tk.BOTH, expand=1)
+
+        self.time_label =  tk.Label(self.window, text = "00:00")
+        self.time_label.grid(row=0, column=1)
         
         self.canvas = tk.Canvas(self.window, width=WIDTH, height=HEIGHT, highlightthickness=0)
         # self.canvas.pack(fill=tk.BOTH, side=tk.TOP) #Add to window
-        self.canvas.grid(row = 0, column=0, rowspan=3)
+        self.canvas.grid(row = 1, column=0, rowspan=3)
         self.new_game_btn = tk.Button(self.window, text="New Game", font = BTN_FONT, fg="blue2", 
                                       width=BTN_WIDTH, height=BTN_HEIGHT, 
                                       command=self.new_game)
-        self.new_game_btn.grid(row=0, column=1, padx=BTN_PADX)
+        self.new_game_btn.grid(row=1, column=1, padx=BTN_PADX)
 
         self.reset_game_btn = tk.Button(self.window, text="Reset", font = BTN_FONT, fg="SpringGreen3", 
                                         width=BTN_WIDTH, height=BTN_HEIGHT,
                                         command=self.reset_game)
-        self.reset_game_btn.grid(row=1, column=1, padx=BTN_PADX)
+        self.reset_game_btn.grid(row=2, column=1, padx=BTN_PADX)
         self.solve_game_btn = tk.Button(self.window, text="Solve", font = BTN_FONT, fg="firebrick1", 
                                         width=BTN_WIDTH, height=BTN_HEIGHT,
                                         command=self.solve)
-        self.solve_game_btn.grid(row=2, column=1, padx=BTN_PADX)
-
+        self.solve_game_btn.grid(row=3, column=1, padx=BTN_PADX)
 
         self.draw_grid()
 
@@ -93,6 +101,31 @@ class SudokuUI(tk.Frame):
                                          outline="red", tags="cursor")
 
 
+    def victory(self):
+        self.window.after_cancel(self.timer)
+        self.canvas.delete("all")
+        self.canvas.configure(bg="dodger blue")
+        mm, ss = divmod(self.time, 60) 
+        self.canvas.create_text(WIDTH / 2, HEIGHT /  2 - 50, text="Excellent", font=WIN_FONT, fill="white smoke")
+        self.canvas.create_text(WIDTH / 2, HEIGHT /  2 + 50, text="Difficulty    " + self.diff, font=WIN_FONT2, fill="white smoke")
+        self.canvas.create_text(WIDTH / 2, HEIGHT /  2 + 100, text="Time         %02d:%02d" % (mm, ss), font=WIN_FONT2, fill="white smoke")
+        
+
+    def create_board(self, level):
+        difficulty = ["Easy", "Medium", "Hard", "Expert"]
+        self.diff = difficulty[level - 1]
+        
+        self.canvas.delete("all")
+        self.canvas.configure(bg="white")
+        self.draw_grid()
+        self.game.generate_board(1)
+        self.draw_puzzle()
+        if(self.timer != None):
+            self.window.after_cancel(self.timer)
+
+        self.time = 0
+        self.timer = self.window.after(0, self.timer_update)
+
     def new_game(self):
         self.select_menu = tk.Toplevel(self.window)
         self.select_menu.title("Select Difficulty")
@@ -104,28 +137,55 @@ class SudokuUI(tk.Frame):
         self.select_menu.geometry("%dx%d+%d+%d" % (w, h, x , y)) # set menu size and position
         easy_btn = tk.Button(self.select_menu, text="Easy", font = BTN_FONT, fg="lime green",
                             width=BTN_WIDTH, height=BTN_HEIGHT,
-                            command=lambda: [self.game.generate_board(2), self.draw_puzzle()])
+                            command=lambda: self.create_board(1))
         easy_btn.pack()
         medium_btn = tk.Button(self.select_menu, text="Medium", font = BTN_FONT, fg="dodger blue",
                                width=BTN_WIDTH, height=BTN_HEIGHT, 
-                               command=lambda: [self.game.generate_board(3), self.draw_puzzle()])
+                               command=lambda: self.create_board(2))
         medium_btn.pack()
         hard_btn = tk.Button(self.select_menu, text="Hard", font = BTN_FONT, fg="dark orange",
                              width=BTN_WIDTH, height=BTN_HEIGHT,
-                             command=lambda: [self.game.generate_board(4), self.draw_puzzle()])
+                             command=lambda: self.create_board(3))
         hard_btn.pack()
         expert_btn = tk.Button(self.select_menu, text="Expert", font = BTN_FONT, fg="red3",
                                width=BTN_WIDTH, height=BTN_HEIGHT,
-                               command=lambda: [self.game.generate_board(5), self.draw_puzzle()])
+                               command=lambda: self.create_board(4))
         expert_btn.pack()
     
     # Clear board
     def reset_game(self):
         self.canvas.delete("numbers")
+    
 
     def solve(self):
-        pass
+        if(self.__solve_backtrack(0, 0)):
+            self.victory()
+        
 
+    def __solve_backtrack(self, i, j):
+        print(i, j)
+        if(j > 8):
+            j = 0
+            i += 1
+            if(i > 8):
+                return True
+        if(self.game.answer_board[i][j] != 0):
+            if(self.__solve_backtrack(i, (j + 1))):
+                return True
+        else:
+            for val in range(1, 10):
+                if(self.game.validate_cell(self.game.answer_board, val, i, j)):
+                    self.game.answer_board[i][j] = val
+
+                    self.draw_puzzle()
+                    time.sleep(0.2)
+                    self.window.update()
+
+                    if(self.__solve_backtrack(i, (j + 1))):
+                        return True
+
+                    self.game.answer_board[i][j] = 0
+        return False
 
     def cell_clicked(self, event):
         x, y = event.x, event.y
@@ -148,3 +208,13 @@ class SudokuUI(tk.Frame):
             self.col, self.row = -1, -1
             self.draw_puzzle()
             self.draw_cursor()
+            if(self.game.check_win()):
+                print("Win")
+                self.victory()
+
+    def timer_update(self):
+        self.time += 1
+        mm, ss = divmod(self.time, 60)
+        self.time_label.configure(text="%02d:%02d" % (mm, ss))
+
+        self.timer = self.window.after(1000, self.timer_update)
